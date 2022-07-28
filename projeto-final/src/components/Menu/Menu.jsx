64aeeabc41c227a87adef647a831728826ce332c
Assuéria <ellyassueria@gmail.com>
2022-07-28 12:06:12 -0300

    Conteúdo da página inicial inserido e estilizado.

--- projeto-final/src/components/Menu/Menu.jsx
+++ projeto-final/src/components/Menu/Menu.jsx
@@ -9,22 +9,22 @@ function Menu() {
             <li className="logo">
                 <Link className="link" to="/"><img src={logo} alt="Logo da papel mulher, um picel em amarelo e branco, que representa o material usado para colar os lambes."/> </Link>
             </li>
-            <li className="item">
+            <li className="unidade">
                 <Link className="link" to="/">Início</Link>
             </li>
-            <li className="item">
+            <li className="unidade">
                 <Link className="link" to="/sobre">Sobre nós</Link>
             </li>
-            <li className="item">
+            <li className="unidade">
                 <a href='#' target='_blank'>Lojinha</a>
             </li>
-            <li className="item">
+            <li className="unidade">
                 <Link className="link" to="/contribua">Contribua</Link>
             </li>
-            <li className="item">
+            <li className="unidade">
                 <a href='https://www.instagram.com/papel.mulher/' target='_blank'>Participe</a>
             </li>
-            <li className="item">
+            <li className="unidade">
                 <Link className="link" to="/ondeestivemos">Onde estivemos</Link>
             </li>
         </ul>
--- projeto-final/src/pages/Home/Home.jsx
+++ projeto-final/src/pages/Home/Home.jsx
@@ -1,13 +1,91 @@
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
-
+import './home.css'
+import imgsection1 from "../../assets/imgsection1.png"
 
 function Home(){
     return(
         <>
-                <Header />
-                <Footer />
-                 
+            <Header />
+
+                <section className="secao1">
+                    <div className="apresentacao">
+                        <h2>
+                                Qual o papel da Papel Mulher?
+                        </h2>
+
+                        <div className="informacoes">
+                            <p className="box1">
+                            A coletiva nasce da ideia de que existe uma disputa de narrativa,
+                            em especial no campo literário, que reconhece certos discursos, e 
+                            outros não.  Afinal o que é literatura, quem pode produzir literatura? 
+                            As mulheres escrevem? São publicadas? Sobre o que escrevem? A 
+                            Papel Mulher nasce da certeza de que as mulheres escrevem por 
+                            anos, apenas não foram e ainda não são reconhecidas como 
+                            escritoras.
+                            <a>
+                                Ver mais
+                            </a>
+                            </p>
+
+                            <img src={imgsection1}/>
+                        </div>
+                    </div>
+                    
+                </section>
+                <section className="secao2">
+                
+                    <h2>
+                        Nossas<span>diretrizes</span>
+                    </h2>
+                    <div className="conteudo">
+                        
+                            <div className="item">
+                                <h3>
+                                    Nosso objetivo
+                                </h3>
+                                <p>
+                                é encher as ruas e a vida das pessoas com palavras 
+                                e histórias de mulheres pois acreditamos, assim, 
+                                criar um mundo melhor, um mundo que tenha e 
+                                respeite muitas e diversas vozes.
+                                </p>
+                            </div>
+                            <div className="item">
+                                <h3>
+                                    Não toleramos
+                                </h3>
+                                <p>
+                                    nenhum tipo de violência, seja ela física ou 
+                                    psicológica entre as membras da coletiva.
+                                </p>
+                            </div>  
+                                           
+                        
+                            <div className="item">
+                                <h3>
+                                    Não permitimos
+                                </h3>
+                                <p>
+                                    discursos de ódio, nem nenhum comportamento que 
+                                    possa ser reconhecido como transfóbico, 
+                                    homofóbico, machista, etc.
+                                </p>
+                            </div>
+                            <div className="item">
+                                <h3>
+                                    Não está liberada
+                                </h3>
+                                <p>
+                                    a vendas dos lambes e produtos que não sejam 
+                                    feitos pela organização da coletiva.
+                                </p>
+                            </div>
+                        
+                    </div>
+                </section>
+
+            <Footer /> 
         </>
     )
 }
